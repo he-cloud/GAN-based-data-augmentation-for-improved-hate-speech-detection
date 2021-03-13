17f7ab62f64a36600963dad8e3671d504f6550e8
he-cloud <59599135+he-cloud@users.noreply.github.com>
2021-03-13 14:20:25 +0100

    Delete script.py

--- Data set/script.py
+++
@@ -1,9 +0,0 @@
-
-data = []
-file_handle = open("train_set1.txt", mode ="w", encoding = 'utf-8')
-for line in open("train.txt", encoding='utf-8'):
-    words = line.strip().split()
-    if words[-1] == "1":
-       line = " ".join(words[0:-1])
-       print(line)
-       file_handle.write(line+"\n")
